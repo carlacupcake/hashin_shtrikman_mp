56da1b9d646877af8db7e69ee2b8c2e6ce5bedc7
Carla Becker <carla.joy.becker@gmail.com>
2025-01-22 16:24:01 -0800

    all tests pass test_genetic_algorithm_result

---
+++ tests/core/genetic_algorithm/test_genetic_algorithm_result.py
@@ -0,0 +1,27 @@
+"""test_genetic_algorithm_result.py"""
+from hashin_shtrikman_mp.core.genetic_algorithm.genetic_algorithm_parameters import GeneticAlgorithmParams
+from hashin_shtrikman_mp.core.genetic_algorithm.genetic_algorithm_result import GeneticAlgorithmResult
+from hashin_shtrikman_mp.core.genetic_algorithm.population import Population
+from hashin_shtrikman_mp.core.genetic_algorithm.optimization_params import OptimizationParams
+import numpy as np
+
+def test_genetic_algorithm_result_initialization():
+    algo_params = GeneticAlgorithmParams()
+    opt_params = OptimizationParams()
+    final_pop = Population(opt_params, algo_params)
+    lowest_costs = np.array([1.0, 0.8, 0.5])
+    avg_parent_costs = np.array([1.2, 1.0, 0.7])
+
+    result = GeneticAlgorithmResult(
+        algo_parameters=algo_params,
+        opt_parameters=opt_params,
+        final_population=final_pop,
+        lowest_costs=lowest_costs,
+        avg_parent_costs=avg_parent_costs
+    )
+
+    assert result.algo_parameters == algo_params
+    assert result.optimization_params == opt_params
+    assert result.final_population == final_pop
+    assert np.array_equal(result.lowest_costs, lowest_costs)
+    assert np.array_equal(result.avg_parent_costs, avg_parent_costs)
